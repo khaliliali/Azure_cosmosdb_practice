2ebe975b03c49fa131f62edca4be97717653a705
khaliliali <ali.khalili@outlook.com>
2023-06-03 13:22:50 +0800

    Adding items in CosmosDB and add Order Class

--- Azure_CosmosDB/Program.cs
+++ Azure_CosmosDB/Program.cs
@@ -30,3 +30,31 @@ string containerName = "Orders";
 
 //    Console.WriteLine("Container created...");
 //}
+
+/* Adding items in CosmosDB */
+
+// await AddItems("01", "Laptop", 133);
+// await AddItems("02", "PC", 532);
+// await AddItems("03", "Mobile", 1300);
+// await AddItems("04", "Laptop", 13);
+
+// async Task AddItems(string orderId, string category, int quantity)
+// {
+//     CosmosClient cosmosClient = new CosmosClient(cosmosEndpointUri, cosmosDBKey);
+
+//     Database database = cosmosClient.GetDatabase(databaseName);
+//     Container container = database.GetContainer(containerName);
+
+//     Order order = new Order()
+//     {
+//         id = Guid.NewGuid().ToString(), // important
+//         orderId = orderId,
+//         category = category,
+//         quantity = quantity
+//     };
+
+//     ItemResponse<Order> response = await container.CreateItemAsync<Order>(order, new PartitionKey(category));
+//     Console.WriteLine("Added item with Order Id {0}", orderId);
+//     Console.WriteLine("Request Unit (Ru) {0}", response.RequestCharge);
+
+// }
