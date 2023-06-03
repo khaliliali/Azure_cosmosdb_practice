4333178d57bf3403867ff1ce7d81d18613311994
khaliliali <ali.khalili@outlook.com>
2023-06-03 13:20:45 +0800

    Create  CosmosDb

---
+++ Azure_CosmosDB/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Azure_CosmosDB
+{
+	public class Order
+	{
+		public string id { get; set; }
+		public string orderId { get; set; }
+		public string category { get; set; }
+		public int quantity { get; set; }
+	}
+}
+
